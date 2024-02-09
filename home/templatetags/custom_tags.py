a178243a80355e2f8940d75234db5ced12a15ea2
rupam office <rupamdhara.1579@gmail.com>
2024-02-09 19:09:40 +0530

    Update

--- home/templatetags/custom_tags.py
+++ home/templatetags/custom_tags.py
@@ -5,7 +5,7 @@ register = template.Library()
 
 @register.filter
 def get_count(value):
-    notebook = NoteBook.objects.get(id=value)
-    note_count = Note.objects.filter(notebook_id=notebook.id).count()
-    return note_count
+    subject = Subject.objects.get(id=value)
+    count = Chapter.objects.filter(subject_id=subject.id).count()
+    return count
 
